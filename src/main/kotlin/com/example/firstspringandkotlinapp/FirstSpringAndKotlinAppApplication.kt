a08eac12e3279e4bfb55bef07347c3fc78541261
daniellaga84@gmail.com <Erkano84!!>
2021-04-12 14:18:55 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/firstspringandkotlinapp/FirstSpringAndKotlinAppApplication.kt
@@ -0,0 +1,11 @@
+package com.example.firstspringandkotlinapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstSpringAndKotlinAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<FirstSpringAndKotlinAppApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/firstspringandkotlinapp/FirstSpringAndKotlinAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.firstspringandkotlinapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FirstSpringAndKotlinAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
