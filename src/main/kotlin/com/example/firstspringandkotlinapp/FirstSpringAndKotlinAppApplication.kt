c3847abc17b641d73bc1817c602a779a702bec18
daniellaga84@gmail.com <Erkano84!!>
2021-04-12 14:31:44 +0200

    Added HtmlController.kt to make it working. Added 3 mustache patterns.

--- src/main/kotlin/com/example/firstspringandkotlinapp/FirstSpringAndKotlinAppApplication.kt
+++ src/main/kotlin/com/example/firstspringandkotlinapp/FirstSpringAndKotlinAppApplication.kt
@@ -1,5 +1,6 @@
 package com.example.firstspringandkotlinapp
 
+import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,7 @@ import org.springframework.boot.runApplication
 class FirstSpringAndKotlinAppApplication
 
 fun main(args: Array<String>) {
-    runApplication<FirstSpringAndKotlinAppApplication>(*args)
+    runApplication<FirstSpringAndKotlinAppApplication>(*args) {
+        setBannerMode(Banner.Mode.OFF)
+    }
 }
