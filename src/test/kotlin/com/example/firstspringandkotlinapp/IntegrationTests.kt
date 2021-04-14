b5c09bc0705e92f46f35172f6a52b425348ad0b6
daniellaga84@gmail.com <Erkano84!!>
2021-04-14 10:33:52 +0200

    Added some body to article.mustache. Added BlogConfiguration.kt with configuration and user. Written tests to check if everything is working.

--- src/test/kotlin/com/example/firstspringandkotlinapp/IntegrationTests.kt
+++ src/test/kotlin/com/example/firstspringandkotlinapp/IntegrationTests.kt
@@ -1,5 +1,6 @@
 package com.example.firstspringandkotlinapp
 
+import com.example.firstspringandkotlinapp.extensions.toSlug
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
@@ -23,12 +24,16 @@ class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
         println(">> Assert blog page title, content and status code")
         val entity = restTemplate.getForEntity<String>("/")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(entity.body).contains("<h1>Blog</h1>")
+        assertThat(entity.body).contains("<h1>Blog</h1>", "Reactor")
     }
 
     @Test
     fun `Assert article page title, contenet and status code`() {
-        println(">> TODO")
+        println(">> Assert article page title, content and status code")
+        val title = "Reactor Aluminium has landed"
+        val entity = restTemplate.getForEntity<String>("/article/${title.toSlug()}")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains(title, "Lorem ipsum", "dolor sot amet")
     }
 
     @AfterAll
